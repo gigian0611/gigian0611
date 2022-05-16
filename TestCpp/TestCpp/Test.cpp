726c44a115933a98672390f45ef9b7859ad75ba5
hkim <hkim@mysoftwiz.com>
2022-05-16 15:15:11 +0900

    면접

--- TestCpp/TestCpp/Test.cpp
+++ TestCpp/TestCpp/Test.cpp
@@ -6,51 +6,26 @@
 #pragma warning (disable : 4996)
 using namespace std;
 
-/*
-상근이는 요즘 설탕공장에서 설탕을 배달하고 있다. 상근이는 지금 사탕가게에 설탕을 정확하게 N킬로그램을 배달해야 한다.
-설탕공장에서 만드는 설탕은 봉지에 담겨져 있다. 봉지는 3킬로그램 봉지와 5킬로그램 봉지가 있다.
+string solution(vector<string> participant, vector<string> completion) {
+    map<string, int> a;
+    string res;
+    for (size_t i = 0; i < participant.size(); i++)
+        a.insert({ participant[i], i });
 
-상근이는 귀찮기 때문에, 최대한 적은 봉지를 들고 가려고 한다. 예를 들어, 18킬로그램 설탕을 배달해야 할 때,
-3킬로그램 봉지 6개를 가져가도 되지만, 5킬로그램 3개와 3킬로그램 1개를 배달하면, 더 적은 개수의 봉지를 배달할 수 있다.
+    for (size_t i = 0; i < completion.size(); i++)
+        a.erase(a.find(completion[i]));
 
-상근이가 설탕을 정확하게 N킬로그램 배달해야 할 때, 봉지 몇 개를 가져가면 되는지 그 수를 구하는 프로그램을 작성하시오.
-
-N은 (3 ≤ N ≤ 5000)
-상근이가 배달하는 봉지의 최소 개수를 출력한다. 만약, 정확하게 N킬로그램을 만들 수 없다면 -1을 출력한다.
-*/
-
-int solution(int n)
-{
-    int A = n / 5; // 5kg 사용 최대 개수
-    int m = 0;
-    int res = 0;
-    while (A >= 0)
-    {
-        m = 0;
-        res = 0;
-        
-        if (A > 0)
-            m = n - (5 * A);
-        else
-            m = n;
-
-        res = m / 3;
-        m = m % 3;
-
-        if (m == 0)
-            return A + res;
-        A--;
-    }
-
-    if (m != 0)
-        return -1;
-
-    return 0;
+    for (auto i : a)
+        res = i.first;
+   
+    return res;
 }
 int main()
 {
+    vector<string> a{ "marina", "josipa", "nikola", "vinko", "filipa" };
+    vector<string> b{ "josipa", "filipa", "marina", "nikola" };
 
-    int ss = solution(11);
+    string ss = solution(a,b);
 
     return 0;
 }
